1baaab6216324ccc7dad077852dcd13ea401de66
Becca <rrdoukas@gmail.com>
2020-09-11 13:10:56 -0500

    adds reviews and professor pages

---
+++ src/views/ProfessorsIndex.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="professor">
+    <h1>Professors</h1>
+    <div v-for="professor in professors">
+      <h2>{{ professor.name }}</h2>
+      <h3>{{professor.school}}</h3>
+      <h3><button v-on:click="`/professors/${professor.id}`">Click here for more information</button></h3>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      professors: [],
+    };
+  },
+  created: function() {
+    this.indexProfessors();
+  },
+  methods: {
+    indexProfessors: function() {
+      axios.get("/api/professor").then(response => {
+        console.log("All professors", response);
+        this.professors = response.data;
+      });
+    },
+  },
+};
